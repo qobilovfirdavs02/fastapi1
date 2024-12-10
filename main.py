1f914b54388124f8b2b559b7d6416f7a9ffbf8c0
Firdavs Qobilov Komilovich <119730654+qobilovfirdavs02@users.noreply.github.com>
2024-12-10 12:06:48 +0300

    first deploying

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}  # Bu yerda asosiy endpoint mavjud
+
+@app.get("/current_time")
+def get_current_time():
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return {"current_time": current_time}
